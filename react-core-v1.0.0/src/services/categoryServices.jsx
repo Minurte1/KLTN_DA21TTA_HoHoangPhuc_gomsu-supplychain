ddeb7d839c24aba77903933b97e87b839f91a0af
Minurte1 <hohoangphucjob@gmail.com>
2025-05-06 15:21:43 +0700

    ádasd

---
+++ react-core-v1.0.0/src/services/categoryServices.jsx
@@ -0,0 +1,37 @@
+import axiosInstance from "../authentication/axiosInstance";
+
+const CATEGORY_API = `${process.env.REACT_APP_URL_SERVER}/categories`;
+
+const categoryServices = {
+  // Lấy danh sách tất cả danh mục
+  getCategories: async () => {
+    const res = await axiosInstance.get(CATEGORY_API);
+    return res.data;
+  },
+
+  // Lấy danh mục theo ID
+  getCategoryById: async (id) => {
+    const res = await axiosInstance.get(`${CATEGORY_API}/${id}`);
+    return res.data;
+  },
+
+  // Tạo danh mục mới
+  createCategory: async (data) => {
+    const res = await axiosInstance.post(CATEGORY_API, data);
+    return res.data;
+  },
+
+  // Cập nhật danh mục
+  updateCategory: async (id, data) => {
+    const res = await axiosInstance.put(`${CATEGORY_API}/${id}`, data);
+    return res.data;
+  },
+
+  // Xóa danh mục
+  deleteCategory: async (id) => {
+    const res = await axiosInstance.delete(`${CATEGORY_API}/${id}`);
+    return res.data;
+  },
+};
+
+export default categoryServices;
---
+++ react-core-v1.0.0/src/services/productionStepServices.jsx
@@ -0,0 +1,37 @@
+import axiosInstance from "../authentication/axiosInstance";
+
+const PRODUCTION_STEP_API = `${process.env.REACT_APP_URL_SERVER}/production_steps`;
+
+const productionStepServices = {
+  // Lấy danh sách tất cả các bước sản xuất
+  getProductionSteps: async () => {
+    const res = await axiosInstance.get(PRODUCTION_STEP_API);
+    return res.data;
+  },
+
+  // Lấy bước sản xuất theo ID
+  getProductionStepById: async (id) => {
+    const res = await axiosInstance.get(`${PRODUCTION_STEP_API}/${id}`);
+    return res.data;
+  },
+
+  // Tạo bước sản xuất mới
+  createProductionStep: async (data) => {
+    const res = await axiosInstance.post(PRODUCTION_STEP_API, data);
+    return res.data;
+  },
+
+  // Cập nhật bước sản xuất
+  updateProductionStep: async (id, data) => {
+    const res = await axiosInstance.put(`${PRODUCTION_STEP_API}/${id}`, data);
+    return res.data;
+  },
+
+  // Xóa bước sản xuất
+  deleteProductionStep: async (id) => {
+    const res = await axiosInstance.delete(`${PRODUCTION_STEP_API}/${id}`);
+    return res.data;
+  },
+};
+
+export default productionStepServices;
