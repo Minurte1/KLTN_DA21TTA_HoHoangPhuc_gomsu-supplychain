a2b90a3f1f07679910155ab7e22cf4da9e702ab0
Minurte1 <hohoangphucjob@gmail.com>
2025-08-14 01:10:44 +0700

    ádasd

--- react-core-v1.0.0/src/web-view/component-view/header.jsx
+++ react-core-v1.0.0/src/web-view/component-view/header.jsx
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import "../component-view/scss/header.scss";
 import logo from "../../public/images/logo.png";
-
+import ReduxExportUseAuthState from "../../redux/redux-export/useAuthServices";
+import axios from "axios";
+import { logout } from "../../redux/authSlice";
+import { useDispatch } from "react-redux";
+import Cookies from "js-cookie";
+import { Menu, MenuItem } from "@mui/material";
 const Header = () => {
   const navigate = useNavigate();
-  const location = useLocation(); // Lấy URL hiện tại
+  const location = useLocation();
+  const { userInfo } = ReduxExportUseAuthState();
 
-  // Hàm kiểm tra URL hiện tại có trùng với menu không
+  const dispatch = useDispatch();
   const isActive = (path) => location.pathname === path;
 
+  const api = process.env.REACT_APP_URL_SERVER;
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = async () => {
+    try {
+      await axios.post(`${api}/logout`);
+      Cookies.remove("accessToken");
+      dispatch(logout());
+      navigate("/login");
+    } catch (error) {
+      console.error("Error during logout:", error);
+    }
+  };
   return (
     <header className="main-header">
       <div className="container">
@@ -55,9 +84,43 @@ const Header = () => {
         {/* Search + Account */}
         <div className="header-actions">
           <input type="text" placeholder="Tìm kiếm..." />
-          <button onClick={() => navigate("/account")} className="account-btn">
-            Tài khoản
-          </button>
+
+          {userInfo ? (
+            <div className="account-dropdown" style={{ position: "relative" }}>
+              <button onClick={handleClick} className="account-btn">
+                {userInfo?.HO_TEN || "Không xác định"}
+              </button>
+
+              <Menu
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "right",
+                }}
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+              >
+                <MenuItem
+                  onClick={() => {
+                    navigate("/profile");
+                    handleClose();
+                  }}
+                >
+                  Tài khoản
+                </MenuItem>
+                <MenuItem onClick={handleLogout}>Đăng xuất</MenuItem>
+              </Menu>
+            </div>
+          ) : (
+            <button onClick={() => navigate("/login")} className="account-btn">
+              Tài khoản
+            </button>
+          )}
+
           <button onClick={() => navigate("/cart")} className="cart-btn">
             Giỏ hàng
           </button>
